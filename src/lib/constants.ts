574c062c3fe18fb71546d99b6a6c9111fbf21820
Musa Dinc <musadinc7@gmail.com>
2023-11-21 16:11:55 +0300

    initial commit

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,7 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
-};
+  github: 'https://github.com/dncmusa',
+  linkedin: 'https://www.linkedin.com/in/dincmusa/',
+  medium: 'https://medium.com/@musadinc7',
+  CodeSandbox: 'https://codesandbox.io/u/dncmusa',
+  Codepen: 'https://codepen.io/dncmusa',
+}
